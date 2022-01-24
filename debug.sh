fa568eeff0db04109347d4bfba6f72b3d3949b72
Moulick Aggarwal <moulickaggarwal@gmail.com>
2022-01-24 14:59:48 +0530

    formating and updates for many functions

--- debug.sh
+++ debug.sh
@@ -2,11 +2,14 @@
 
 def_cap="default"
 pod_name="$(id -un | tr '.' '-')-debug"
-alias kpf="kubectl port-forward"
+read_only_creds_secret_name=rds-readonly-credentials
 
+urlencode_old() {
+  if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <string>" >&2
+    return 1
+  fi
 
-urlencode() {
-  # urlencode <string>
   old_lc_collate=$LC_COLLATE
   LC_COLLATE=C
   local length="${#1}"
@@ -18,10 +21,24 @@ urlencode() {
     esac
   done
   LC_COLLATE=$old_lc_collate
+
+}
+
+urlencode() {
+  if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <string>" >&2
+    return 1
+  fi
+  # Jq can also do the encoding for us, and is probably more robust than the above
+  printf %s "$1" | jq -sRr @uri
 }
 
 urldecode() {
-  # urldecode <string>
+  if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <string>" >&2
+    return 1
+  fi
+
   local url_encoded="${1//+/ }"
   printf '%b' "${url_encoded//%/\\x}"
 }
@@ -38,10 +55,17 @@ kdebug() {
   fi
 }
 
+kdebug-kill() {
+  echo "\ufb81 Killing pod $pod_name"
+  kubectl delete pod "$pod_name" -n $def_cap --grace-period=0 --force --ignore-not-found
+}
+
 db_shell() {
   if [ "$#" -ne 5 ]; then
-    echo "Usage: $0 user pass url port db_name" >&2
+    echo "Usage: $0 <user> <urlencoded password> <url> <port> <db_name>" >&2
     return 1
+  else
+    echo "launching pod shell"
   fi
 
   user=$1
@@ -59,14 +83,14 @@ db_shell() {
   else
     # tput setaf 1 = red
     echo "$(tput setaf 1)Pod not found or dead 😢, making a new one $(tput sgr0)"
-    kubectl delete pod "$pod_name" --ignore-not-found -n $def_cap --grace-period=0 --force --ignore-not-found
+    kubectl delete pod "$pod_name" -n $def_cap --grace-period=0 --force --ignore-not-found
     kubectl run -it --restart=Never "$pod_name" --image=moulick/debug-image:latest -n $def_cap -- psql "$postg"
   fi
 }
 
 create-readonly-user() {
   if [ "$#" -ne 5 ]; then
-    echo "Usage: $0 user pass url port db_name" >&2
+    echo "Usage: $0 <user> <urlencoded password> <url> <port> <db_name>" >&2
     return 1
   fi
 
@@ -78,6 +102,8 @@ create-readonly-user() {
 
   postg="postgresql://$user:$pass@$url:$port/$db_name"
 
+  # disbaling the shellchecks as this is like embeded SQL Query
+  # shellcheck disable=SC2016,SC2089
   SQLCMD='DO
   $do$
   BEGIN
@@ -96,7 +122,7 @@ create-readonly-user() {
   GRANT USAGE ON ALL SEQUENCES IN SCHEMA public TO readonly_user;
   -- GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO readonly_user;
   ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT USAGE ON SEQUENCES TO readonly_user;'
-  echo $SQLCMD
+  echo "$SQLCMD"
 
   pod_status=$(kubectl get pods "$pod_name" -n $def_cap -o=jsonpath='{.status.phase}')
 
@@ -105,7 +131,7 @@ create-readonly-user() {
   else
     # tput setaf 1 = red
     echo "$(tput setaf 1)Pod not found or dead 😢, making a new one $(tput sgr0)"
-    kubectl delete pod "$pod_name" --ignore-not-found -n $def_cap --grace-period=0 --force --ignore-not-found
+    kubectl delete pod "$pod_name" -n $def_cap --grace-period=0 --force --ignore-not-found
     kubectl run -it --restart=Never "$pod_name" --image=moulick/debug-image:latest -n $def_cap -- psql "$postg" -c "$SQLCMD"
   fi
 }
@@ -115,15 +141,22 @@ alfresco() {
 }
 
 in-cluster-post() {
-  secret=$(kubectl get secret "$1-main-postgresql" -n "${1}" -o json | jq -r '.data | map_values(@base64d)')
-  if [[ -z $secret ]]; then
-    echo "could not connect to $1"
+  if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <name>" >&2
     return 1
   fi
 
-  escaped_password=$(urlencode "$(echo -E "$secret" | jq -r '.MAIN_PASSWORD')")
+  secret=$(kubectl get secret "$1-main-postgresql" -n "${1}" -o json | jq -er '.data | map_values(@base64d)')
+  if [[ -z $secret ]]; then
+    echo "could not connect to $1"
+    return 1
+  else
+    echo "got secret from cluster"
+  fi
 
-  user=$(echo -E "$secret" | jq -r '.MAIN_USERNAME')
+  escaped_password=$(urlencode "$(echo -E "$secret" | jq -er '.MAIN_PASSWORD')")
+
+  user=$(echo -E "$secret" | jq -er '.MAIN_USERNAME')
   addr="helm-$1-main-postgresql.${1/-(master|develop|v1)/}"
   port="5432"
   db_name="main"
@@ -133,15 +166,17 @@ in-cluster-post() {
 }
 
 in-cluster-post-create-readonly-user() {
-  secret=$(kubectl get secret "$1-main-postgresql" -n "${1}" -o json | jq -r '.data | map_values(@base64d)')
+  secret=$(kubectl get secret "$1-main-postgresql" -n "${1}" -o json | jq -er '.data | map_values(@base64d)')
   if [[ -z $secret ]]; then
     echo "could not connect to $1"
     return 1
+  else
+    echo "got secret from cluster"
   fi
 
-  escaped_password=$(urlencode "$(echo -E "$secret" | jq -r '.MAIN_PASSWORD')")
+  escaped_password=$(urlencode "$(echo -E "$secret" | jq -er '.MAIN_PASSWORD')")
 
-  user=$(echo -E "$secret" | jq -r '.MAIN_USERNAME')
+  user=$(echo -E "$secret" | jq -er '.MAIN_USERNAME')
   addr="helm-$1-main-postgresql.${1}"
   port="5432"
   db_name="main"
@@ -150,15 +185,22 @@ in-cluster-post-create-readonly-user() {
   create-readonly-user "$user" "$escaped_password" "$addr" "$port" "$db_name"
 }
 
-
 rds-create-readonly-user() {
+  if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <instance name> <db name>" >&2
+    return 1
+  fi
+
   secret=$(kubectl get secret "$1-$2-rds-instance" -n "${1}" -o json | jq -er '.data | map_values(@base64d)')
   if [[ -z $secret ]]; then
     echo "could not connect to $1-$2"
     return 1
+  else
+    echo "got secret from cluster"
   fi
+
   escaped_password=$(urlencode "$(echo -E "$secret" | jq -er '.MASTER_PASSWORD')")
-  echo -E "$secret" | jq -r '.ENDPOINT_ADDRESS'
+  echo -E "$secret" | jq -er '.ENDPOINT_ADDRESS'
 
   user=$(echo -E "$secret" | jq -er '.MASTER_USERNAME')
   addr=$(echo -E "$secret" | jq -er '.ENDPOINT_ADDRESS')
@@ -168,19 +210,54 @@ rds-create-readonly-user() {
   create-readonly-user "$user" "$escaped_password" "$addr" "$port" "$db_name"
 }
 
+# rds-rw() {
+#   secret=$(kubectl get secret "$1-main-postgresql" -n "${1}" -o json | jq -er '.data | map_values(@base64d)')
+
+#   if [[ -z $secret ]]; then
+#     echo "could not connect to $1"
+#     return 1
+#   fi
+
+#   escaped_password=$(urlencode "$(echo -E "$secret" | jq -er '.MASTER_PASSWORD')")
+#   echo "$secret" | jq -er '.DB_INSTANCE_IDENTIFIER'
+
+#   user=$(echo -E "$secret" | jq -er '.MASTER_USERNAME')
+#   addr=$(echo -E "$secret" | jq -er '.ENDPOINT_ADDRESS')
+#   port=$(echo -E "$secret" | jq -er '.PORT')
+#   db_name=$(echo -E "$secret" | jq -er '.DB_NAME')
+
+#   db_shell "$user" "$escaped_password" "$addr" "$port" "$db_name"
+# }
 
 rds() {
-  secret=$(kubectl get secret "$1-main-postgresql" -n "${1}" -o json | jq -er '.data | map_values(@base64d)')
 
-  if [[ -z $secret ]]; then
-    echo "could not connect to $1"
+  if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <instance name> <db name>" >&2
     return 1
   fi
 
-  escaped_password=$(urlencode "$(security find-generic-password -a moulick.aggarwal -s postgres-readonly -w)")
-  echo -E "$secret" | jq -r '.DB_INSTANCE_IDENTIFIER'
+  secretmanager_secret_name="/$ENVIRONMENT/$STAGE/$read_only_creds_secret_name"
 
-  user="readonly_user"
+  secret=$(kubectl get secret "$1-$2-rds-instance" -n "${1}" -o json | jq -er '.data | map_values(@base64d)')
+  if [[ -z $secret ]]; then
+    echo "could not connect to $1-$2"
+    return 1
+  else
+    echo "got secret from cluster"
+  fi
+
+  readonly_creds=$(aws secretsmanager get-secret-value --secret-id "$secretmanager_secret_name" --output json | jq -er '.SecretString')
+  if [[ -z $readonly_creds ]]; then
+    echo "could not find read only secret $secretmanager_secret_name in aws secretmanager "
+    return 1
+  else
+    echo "got read_only secret from aws"
+  fi
+
+  escaped_password=$(urlencode "$(echo -E "$readonly_creds" | jq -er .RDS_READONLY_USER_PASSWORD)")
+  echo -E "$secret" | jq -er '.ENDPOINT_ADDRESS'
+
+  user=$(echo -E "$readonly_creds" | jq -er .RDS_READONLY_USER)
   addr=$(echo -E "$secret" | jq -er '.ENDPOINT_ADDRESS')
   port=$(echo -E "$secret" | jq -er '.PORT')
   db_name=$(echo -E "$secret" | jq -er '.DB_NAME')
@@ -189,15 +266,21 @@ rds() {
 }
 
 rds-rw() {
-  secret=$(kubectl get secret "$1-main-postgresql" -n "${1}" -o json | jq -er '.data | map_values(@base64d)')
+
+  if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <instance name> <db name>" >&2
+    return 1
+  fi
+
+  secret=$(kubectl get secret "$1-$2-rds-instance" -n "${1}" -o json | jq -er '.data | map_values(@base64d)')
 
   if [[ -z $secret ]]; then
-    echo "could not connect to $1"
+    echo "could not connect to $1-$2"
     return 1
   fi
 
   escaped_password=$(urlencode "$(echo -E "$secret" | jq -er '.MASTER_PASSWORD')")
-  echo "$secret" | jq -r '.DB_INSTANCE_IDENTIFIER'
+  echo -E "$secret" | jq -er '.ENDPOINT_ADDRESS'
 
   user=$(echo -E "$secret" | jq -er '.MASTER_USERNAME')
   addr=$(echo -E "$secret" | jq -er '.ENDPOINT_ADDRESS')
@@ -206,60 +289,40 @@ rds-rw() {
 
   db_shell "$user" "$escaped_password" "$addr" "$port" "$db_name"
 }
-
-rds-rw-v3() {
-  secret=$(kubectl get secret "$1-main-rds-instance" -n "${1}" -o json | jq -er '.data | map_values(@base64d)')
-
-  if [[ -z $secret ]]; then
-    echo "could not connect to $1"
-    return 1
-  fi
-
-  escaped_password=$(urlencode "$(echo -E "$secret" | jq -er '.MASTER_PASSWORD')")
-  echo -E "$secret" | jq -r '.ENDPOINT_ADDRESS'
-
-  user=$(echo -E "$secret" | jq -er '.MASTER_USERNAME')
-  addr=$(echo -E "$secret" | jq -er '.ENDPOINT_ADDRESS')
-  port=$(echo -E "$secret" | jq -er '.PORT')
-  db_name=$(echo -E "$secret" | jq -er '.DB_NAME')
-
-  db_shell "$user" "$escaped_password" "$addr" "$port" "$db_name"
-}
-
 
 argo() {
   local_url="http://localhost:8083"
   echo "argo at $local_url"
   open $local_url
-  kpf svc/argocd-server -n capabilities 8083:80
+  kubectl port-forward svc/argocd-server -n argo 8083:80
 }
 
 logs() {
   local_url="http://localhost:5602"
   echo "kibana at $local_url"
   open $local_url
-  kpf svc/kibana-kb-http -n capabilities 5602:5601
+  kubectl port-forward svc/logging-kibana-kb-http -n mla 5602:5601
 }
 
-logs-etoe() {
-  local_url="http://localhost:5603"
-  echo "kibana at $local_url"
-  open $local_url
-  kpf svc/kibana-etoe-kb-http -n capabilities 5603:5601
-}
+# logs-etoe() {
+#   local_url="http://localhost:5603"
+#   echo "kibana at $local_url"
+#   open $local_url
+#   kubectl port-forward svc/kibana-etoe-kb-http -n capabilities 5603:5601
+# }
 
 grafana() {
   local_url="http://localhost:3002"
   echo "grafana at $local_url"
   open $local_url
-  kpf svc/grafana-service -n capabilities 3002:3000
+  kubectl port-forward svc/grafana-service -n mla 3002:3000
 }
 
 glowroot() {
   local_url="http://localhost:4000"
   echo "glowroot at $local_url"
   open $local_url
-  kpf svc/glowroot-collector -n capabilities 4000:4000
+  kubectl port-forward svc/main-glowroot-collector -n mla 4000:4000
 }
 
 start_automation() {
@@ -273,6 +336,11 @@ stop_automation() {
 }
 
 mongo() {
+  if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <instance name>" >&2
+    return 1
+  fi
+
   echo kubectl get secret "$1-resourcedata" -n "${1/-(master|develop|v1)/}" -o json
   secret=$(kubectl get secret "$1-resourcedata" -n "${1/-(master|develop|v1)/}" -o json | jq -er '.data | map_values(@base64d)')
 
@@ -319,7 +387,7 @@ mq() {
     return 1
   fi
   addr=$(echo -E "$secret" | jq -er '.ATMQ_WEB_CONSOLE')
-  domain="${addr/(https:\/\/)/}" # removes https:// from url for socat
+  domain="${addr/(https:\/\/)/}"         # removes https:// from url for socat
   domain_standby="${domain/-1.mq/-2.mq}" # url for MQ is running in active/standby mode
 
   case $1 in
@@ -343,7 +411,7 @@ mq() {
   if [[ "$pod_status" == "Running" ]]; then
     echo "MQ at $local_url"
     open $local_url
-    kpf "$mq_pod" -n "$def_cap" 8163:80
+    kubectl port-forward "$mq_pod" -n "$def_cap" 8163:80
   else
     # tput setaf 1 = red
     echo "$(tput setaf 1)Pod not found or dead 😢 $(tput sgr0)"
@@ -353,10 +421,9 @@ mq() {
     sleep 5
     echo "MQ at $local_url"
     open $local_url
-    kpf "$mq_pod" -n "$def_cap" 8163:80
+    kubectl port-forward "$mq_pod" -n "$def_cap" 8163:80
   fi
 }
-
 
 proxy() {
   if [ "$#" -ne 1 ]; then
@@ -366,11 +433,11 @@ proxy() {
   telepresence_namespace="default"
 
   case $1 in
-    d)
-      echo "Telepresense to D"
-      kdelp --ignore-not-found "telepresense-$pod_name" -n "$telepresence_namespace"
+  d)
+    echo "Telepresense to D"
+    kubectl delete pod --ignore-not-found "telepresense-$pod_name" -n "$telepresence_namespace"
 
-      telepresence \
+    telepresence \
       --also-proxy hc-apigw-d-internal.hilti.com \
       --also-proxy hc-apigw-d.hilti.com \
       --also-proxy hc-webgate-d.hilti.com \
@@ -378,13 +445,13 @@ proxy() {
       --new-deployment "telepresense-$pod_name" \
       --namespace "$telepresence_namespace"
 
-      kdelp --ignore-not-found "telepresense-$pod_name" -n "$telepresence_namespace"
-      ;;
-    q)
-      echo "Telepresense to Q"
-      kdelp --ignore-not-found "telepresense-$pod_name" -n "$telepresence_namespace"
+    kubectl delete pod --ignore-not-found "telepresense-$pod_name" -n "$telepresence_namespace"
+    ;;
+  q)
+    echo "Telepresense to Q"
+    kubectl delete pod --ignore-not-found "telepresense-$pod_name" -n "$telepresence_namespace"
 
-      telepresence \
+    telepresence \
       --also-proxy hc-apigw-q-internal.hilti.com \
       --also-proxy hc-apigw-q.hilti.com \
       --also-proxy hc-webgate-q.hilti.com \
@@ -392,33 +459,33 @@ proxy() {
       --new-deployment "telepresense-$pod_name" \
       --namespace "$telepresence_namespace"
 
-      kdelp --ignore-not-found "telepresense-$pod_name" -n "$telepresence_namespace"
-      ;;
-    p)
-      echo "Telepresense to P"
-      kdelp --ignore-not-found "telepresense-$pod_name" -n "$telepresence_namespace"
+    kubectl delete pod --ignore-not-found "telepresense-$pod_name" -n "$telepresence_namespace"
+    ;;
+  p)
+    echo "Telepresense to P"
+    kubectl delete pod --ignore-not-found "telepresense-$pod_name" -n "$telepresence_namespace"
 
-      telepresence \
+    telepresence \
       --also-proxy cloudapis.hilti.com \
       --also-proxy cloudapis-internal.hilti.com \
       --method=vpn-tcp \
       --new-deployment "telepresense-$pod_name" \
       --namespace "$telepresence_namespace"
 
-      kdelp --ignore-not-found "telepresense-$pod_name" -n "$telepresence_namespace"
-      ;;
-    *)
-      echo "Pure Telepresense"
-      kdelp --ignore-not-found "telepresense-$pod_name" -n "$telepresence_namespace"
+    kubectl delete pod --ignore-not-found "telepresense-$pod_name" -n "$telepresence_namespace"
+    ;;
+  *)
+    echo "Pure Telepresense"
+    kubectl delete pod --ignore-not-found "telepresense-$pod_name" -n "$telepresence_namespace"
 
-      telepresence \
+    telepresence \
       --method=vpn-tcp \
       --new-deployment "telepresense-$pod_name" \
       --namespace "$telepresence_namespace"
 
-      kdelp --ignore-not-found "telepresense-$pod_name" -n "$telepresence_namespace"
-      ;;
-    esac
+    kubectl delete pod --ignore-not-found "telepresense-$pod_name" -n "$telepresence_namespace"
+    ;;
+  esac
 }
 
 es() {
@@ -445,7 +512,7 @@ es() {
   if [[ "$pod_status" == "Running" ]]; then
     echo "ES at $local_url"
     open "$local_url"
-    kpf "$pod" -n "$def_cap" 8085:80
+    kubectl port-forward "$pod" -n "$def_cap" 8085:80
   else
     # tput setaf 1 = red
     echo "$(tput setaf 1)Pod not found or dead 😢 $(tput sgr0)"
@@ -455,7 +522,7 @@ es() {
     sleep 5
     echo "ES at $local_url"
     open "$local_url"
-    kpf "$pod" -n "$def_cap" 8085:80
+    kubectl port-forward "$pod" -n "$def_cap" 8085:80
   fi
 }
 
@@ -465,11 +532,11 @@ waiter() {
   echo "Waiting for localhost:8080"
   while ! (nc -w 3 -z localhost 8080); do
     sleep 1
-      echo -n "."
+    echo -n "."
     if [ $retries -eq $TIMEOUT ]; then
       echo "😱 Port Forward did not start, Timeout, aborting, mayday mayday 😱"
       exit 1
     fi
-    retries=$((retries+1))
+    retries=$((retries + 1))
   done
 }
